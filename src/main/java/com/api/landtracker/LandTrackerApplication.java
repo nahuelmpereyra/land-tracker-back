916946830e7c6be76c95d17df7db2738cdb3ef16
nahuelmpereyra <nahuelmpereyra@gmail.com>
2023-08-31 22:05:43 -0300

    commit inicial

---
+++ src/main/java/com/api/landtracker/LandTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.api.landtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/landtracker/LandTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.landtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
